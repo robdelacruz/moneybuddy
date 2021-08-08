e8543f90c14b1d695e6cf7de78c8cf0b25cea99c
rob <robdelacruz@gmail.com>
2021-08-09 01:56:00 +0800

    add filter to accounts and txns

--- Accounts.svelte
+++ Accounts.svelte
@@ -3,15 +3,22 @@
         <h1 class="text-sm font-bold">Accounts</h1>
         <a class="text-xs pill" href="/" on:click|preventDefault={oncreate}>Create</a>
     </div>
-{#if root == null}
+{#if root == null || ui.accounts == null}
     <p class="fg-dim">No data</p>
 {:else}
+    {#if ui.editid == -1}
+        <div class="mb-2">
+            <form autocomplete="off" on:submit|preventDefault="{e => {}}">
+                <input class="block bg-input fg-normal py-1 px-2 w-full" name="filter" id="filter" type="text" placeholder="Filter" bind:value={ui.frm.filter} on:keyup={processFilter}>
+            </form>
+        </div>
+    {/if}
     {#if ui.editid == 0}
         <div class="p-2 border-b border-cell">
             <AccountForm account={ui.newaccount} currencies={root.currencies} on:submit={accountform_done} on:cancel={accountform_done} />
         </div>
     {/if}
-    {#each root.accounts as account (account.accountid)}
+    {#each ui.accounts as account (account.accountid)}
         {#if ui.editid == account.accountid}
         <div class="p-2 border-b border-cell">
             <AccountForm account={account} currencies={root.currencies} on:submit={accountform_done} on:cancel={accountform_done} />
@@ -42,15 +49,27 @@ export let root = null;
 
 let svcurl = "/api";
 let ui = {};
-ui.selid = 0;
-ui.editid = -1;
-ui.newaccount = {
-    accountid: 0,
-    code: "",
-    name: "",
-    accounttype: 0,
-    currencyid: 0,
-};
+ui.accounts = null;
+$: if (root != null) {
+    processFilter();
+}
+
+$: init();
+
+function init() {
+    ui.selid = 0;
+    ui.editid = -1;
+    ui.newaccount = {
+        accountid: 0,
+        code: "",
+        name: "",
+        accounttype: 0,
+        currencyid: 0,
+    };
+
+    ui.frm = {};
+    ui.frm.filter = "";
+}
 
 export function reset() {
     ui.selid = 0;
@@ -84,5 +103,25 @@ function accountform_done(e) {
     ui.editid = -1;
 }
 
+function processFilter() {
+    let filter = ui.frm.filter.trim();
+    if (filter == "") {
+        ui.accounts = root.accounts;
+        return;
+    }
+
+    ui.accounts = filterAccounts(root.accounts, filter);
+}
+function filterAccounts(accounts, filter) {
+    filter = filter.toLowerCase();
+    let aa = [];
+    for (let i=0; i < accounts.length; i++) {
+        let a = accounts[i];
+        if (a.name.toLowerCase().includes(filter)) {
+            aa.push(a);
+        }
+    }
+    return aa;
+}
 </script>
 
--- Txns.svelte
+++ Txns.svelte
@@ -1,22 +1,29 @@
 <div class="bg-normal fg-normal mb-2 mr-2 py-2 px-4" style="min-width: 40rem;">
-{#if account == null || account.txns == null}
+{#if account == null || ui.txns == null}
     <p class="fg-dim">Select Account</p>
 {:else}
     <div class="flex flex-row justify-between items-end mb-2">
         <h1 class="text-sm font-bold">Transactions for {account.name}</h1>
         <a class="text-xs pill" href="/" on:click|preventDefault={oncreate}>Create</a>
     </div>
+    {#if ui.editid == -1}
+        <div class="mb-2">
+            <form autocomplete="off" on:submit|preventDefault="{e => {}}">
+                <input class="block bg-input fg-normal py-1 px-2 w-full" name="filter" id="filter" type="text" placeholder="Filter" bind:value={ui.frm.filter} on:keyup={processFilter}>
+            </form>
+        </div>
+    {/if}
     {#if ui.editid == 0}
         <div class="p-2 border-b border-cell">
             <TxnForm txn={ui.newtxn} on:submit={txnform_done} on:cancel={txnform_done} />
         </div>
     {/if}
-    {#each account.txns as t (t.txnid)}
-    {#if ui.editid == t.txnid}
+    {#each ui.txns as t (t.txnid)}
+        {#if ui.editid == t.txnid}
         <div class="p-2 border-b border-cell">
             <TxnForm txn={t} on:submit={txnform_done} on:cancel={txnform_done} />
         </div>
-    {:else if ui.selid == t.txnid}
+        {:else if ui.selid == t.txnid}
         <a href="/" on:click|preventDefault="{e => onedittxn(t)}">
             <div class="flex flex-row flex-start p-1 border-b border-cell highlight-1">
                 <p class="cell-date">{t.date.substring(0, 10)}</p>
@@ -60,16 +67,28 @@ export let account = null;
 
 let svcurl = "/api";
 let ui = {};
-ui.selid = 0;
-ui.editid = -1;
-ui.newtxn = {
-    txnid: 0,
-    accountid: 0,
-    date: "",
-    ref: "",
-    desc: "",
-    amt: 0.0,
-};
+ui.txns = null;
+$: if (account != null) {
+    processFilter();
+}
+
+$: init();
+
+function init() {
+    ui.selid = 0;
+    ui.editid = -1;
+    ui.newtxn = {
+        txnid: 0,
+        accountid: 0,
+        date: "",
+        ref: "",
+        desc: "",
+        amt: 0.0,
+    };
+
+    ui.frm = {};
+    ui.frm.filter = "";
+}
 
 export function reset() {
     ui.selid = 0;
@@ -103,4 +122,24 @@ function txnform_done(e) {
     ui.editid = -1;
 }
 
+function processFilter() {
+    let filter = ui.frm.filter.trim();
+    if (filter == "") {
+        ui.txns = account.txns;
+        return;
+    }
+
+    ui.txns = filterTxns(account.txns, filter);
+}
+function filterTxns(txns, filter) {
+    filter = filter.toLowerCase();
+    let tt = [];
+    for (let i=0; i < txns.length; i++) {
+        let t = txns[i];
+        if (t.desc.toLowerCase().includes(filter)) {
+            tt.push(t);
+        }
+    }
+    return tt;
+}
 </script>
